4770aa6a285e41b52303b5b63c5af5e5b4299fcb
aylin <pintosaylin7@gmail.com>
2024-10-18 16:49:55 +0000

    last digit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - funcion
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
