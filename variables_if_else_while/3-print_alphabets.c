7ecc93e3f95ad78bb15342c79ef8373ba3ab3fb4
aylin <pintosaylin7@gmail.com>
2024-10-22 13:28:00 +0000

    print cap

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char variable, cap;
+
+	for (variable = 'a'; variable <= 'z'; variable++)
+		putchar(variable);
+	for (cap = 'A'; cap <= 'Z'; cap++)
+		putchar(cap);
+
+{
+		putchar('\n');
+}
+	return (0);
+	}
