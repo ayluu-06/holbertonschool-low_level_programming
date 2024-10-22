be64df7ed06b4aab373aa4874a0262326fcc6013
aylin <pintosaylin7@gmail.com>
2024-10-22 18:17:07 +0000

    print

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char vari;
+
+	for (vari = 'z'; vari <= 'a'; vari--)
+		putchar(vari);
+	putchar('\n');
+	return (0);
+}
