88978968e32c5b54deb566639a7945bdc7c115fc
aylin <pintosaylin7@gmail.com>
2024-10-22 19:04:12 +0000

    print combinacion

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	int numi;
+
+	for (numi = '0'; numi <= '9'; numi++)
+		putchar(numi);
+	putchar('\n');
+
+	return (0);
+}
