a3be52b6917407c4d59ceca9e4605d4aa5d52236
aylin <pintosaylin7@gmail.com>
2024-10-24 14:37:30 +0000

    print x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - print x10 alph
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+
+{
+		char variable;
+		int alp;
+	
+	for (alp = 0; alp <= 9; alp++)
+	{
+	for (variable = 'a'; variable <= 'z'; variable++)
+	       	_putchar(variable);
+	
+	_putchar('\n');
+	}
+}
