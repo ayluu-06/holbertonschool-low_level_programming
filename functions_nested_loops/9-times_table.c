65f112d3e7e628ecc709834051bed18415236cc6
aylin <pintosaylin7@gmail.com>
2024-10-28 02:45:00 +0000

    print 0-98 with coma

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,10 +1,38 @@
 #include "main.h"
 
 /**
- *
+ * times_table - print 9 time table
  *
  */
 
 void times_table(void)
 {
-	int i, j,
+	int h, v, res;
+
+	for (h = 0; h <= 9; h++)
+	{
+		for (v = 0; v <= 9; v++)
+		{
+			res = h * v;
+			if (res > 9)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else
+			{
+				if (v != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(res + '0');
+			}
+			if (v != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
