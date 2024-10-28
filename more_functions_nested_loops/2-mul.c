d8d5c320f3276190489e2692fe66fb76577899b7
aylin <pintosaylin7@gmail.com>
2024-10-28 13:56:20 +0000

    multiply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *mul - multiplies two integers
+ * Return: integersi
+ * @a: variable 1
+ * @b: varibale 2
+ */
+
+int mul(int a, int b)
+{
+	int n = (a * b);
+		return (n);
+}
