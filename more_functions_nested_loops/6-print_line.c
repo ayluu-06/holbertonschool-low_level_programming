15ac8f6f3628a203966113032358ea5b0431c6e6
aylin <pintosaylin7@gmail.com>
2024-10-29 18:30:03 +0000

    print lines

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,25 @@
 #include "main.h"
 
 /**
- *
- *
+ * print_line - prints lines
+ * @n: variable
  */
 
 void print_line(int n)
 {
-	int (*n);
+	int line;
 
-	for (n = 0 )
+	if (n < 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (line = 0; line < n; line++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar ('\n');
+
+}
