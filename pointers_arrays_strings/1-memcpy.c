b4d67ea045981be6273f55928ae544206fececf1
aylin <pintosaylin7@gmail.com>
2024-11-06 20:02:24 +0000

    copy from one to another

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies n bytes from src to dest
+ * @n: number of bytes to copie from area src
+ * @src: memory area where the bytes are gonna be copied
+ * @dest: destiny area to where the bytes are gonna be copied
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *p = src;
+
+	while (n > 0)
+	{
+		n--;
+		*p++ = *src++;
+	}
+	return (dest);
+}
