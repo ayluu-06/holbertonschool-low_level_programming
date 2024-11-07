25943e6371b60559edca32f43d102466b81f7c7f
aylin <pintosaylin7@gmail.com>
2024-11-07 18:03:21 +0000

    chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- *
- *
+ * print_chessboard - prints chessboard
+ * @a: pointer
  *
  */
 
 void print_chessboard(char (*a)[8])
 {
-	char rf;
-	char rc;
+	int rf;
+	int rc;
 
 	for (rf = 0; rf < 8; rf++)
 	{
-		for (rc =; rc < 8; rc++)
+		for (rc = 0; rc < 8; rc++)
 		{
-			printf(a[rf]);
-			printf(a[rc]);
+			_putchar (a[rf][rc]);
+		}
+	_putchar('\n');
 	}
 }
