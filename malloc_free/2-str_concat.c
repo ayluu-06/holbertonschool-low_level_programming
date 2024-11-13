def949898491fb8297fe62db8d768d78d6c359e1
aylin <pintosaylin7@gmail.com>
2024-11-13 14:16:57 +0000

    concad

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two string
+ * @s1: najn
+ * @s2: hsad
+ * Return: uhdiuewh
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int j;
+	char *pointer;
+	int k;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++);
+	for (j = 0; s2[j] != '\0'; j++);
+	
+	pointer = (char *)malloc(i + j + 1 * sizeof(char));
+
+	if (pointer == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+	{
+		pointer[i + j] = s1[k];
+	}
+	for (k = 0; k < j; k++)
+	{
+		pointer[i + j] = s2[k];
+		k++;
+	}
+	pointer[k] = '\0';
+
+	return (pointer);
+
+}
