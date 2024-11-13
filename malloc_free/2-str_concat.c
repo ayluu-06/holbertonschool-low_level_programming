5eebcc5b1f7139b7fd93ff750cdcd847e5678994
aylin <pintosaylin7@gmail.com>
2024-11-13 17:19:09 +0000

    concad

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -22,10 +22,13 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	for (i = 0; s1[i] != '\0'; i++);
-	for (j = 0; s2[j] != '\0'; j++);
-	
-	pointer = (char *)malloc(i + j + 1 * sizeof(char));
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+	pointer = (char *)malloc((i + j + 1) * sizeof(char));
 
 	if (pointer == NULL)
 	{
@@ -33,14 +36,13 @@ char *str_concat(char *s1, char *s2)
 	}
 	for (k = 0; k < i; k++)
 	{
-		pointer[i + j] = s1[k];
+		pointer[k] = s1[k];
 	}
 	for (k = 0; k < j; k++)
 	{
-		pointer[i + j] = s2[k];
-		k++;
+		pointer[i + k] = s2[k];
 	}
-	pointer[k] = '\0';
+	pointer[i + j] = '\0';
 
 	return (pointer);
 
