c0b4be36dc2d9b198cf9a3e55f70f11689ceb7db
Your Name <you@example.com>
2024-11-14 15:05:57 +0000

    matrix

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,19 +2,43 @@
 
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers
- * @width:
- * @height:
- * Return:
+ * @width:iwn
+ * @height:juw
+ * Return:iweubf
  */
 
 int **alloc_grid(int width, int height)
 {
-	int *ptr
+	int **ptr;
+	int i;
+	int j;
+	int k;
+
 	if (width <= 0 || height <= 0)
 	{
-		return (NULL)
+		return (NULL);
 	}
 	ptr = (int **)malloc(height * sizeof(int *));
 	if (ptr == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(width * sizeof(int));
+		if (ptr[i] == NULL)
+		{
+			for (k = 0; k < i; k++)
+			{
+				free(ptr[k]);
+			}
+			free(ptr);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
