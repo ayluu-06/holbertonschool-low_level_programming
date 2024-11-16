52b44ef0cc3b7ad3f04af8f5ff0388bc5b237eb5
Your Name <you@example.com>
2024-11-16 12:39:56 +0000

    initialize

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * @d: d
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
