a76794053e5fc793c67cb7191c1697c5d050fc99
Your Name <you@example.com>
2024-11-16 06:36:28 +0000

    define

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - Define a new type struct dog with the following elements
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
